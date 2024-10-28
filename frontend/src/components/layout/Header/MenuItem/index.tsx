85f4aea5b3765f84dca6ee61b3a9cd4df61a4690
Duke Haymich <71746413+DukeHaymich@users.noreply.github.com>
2024-10-28 09:58:52 +0700

    f

--- frontend/src/components/layout/Header/MenuItem/index.tsx
+++ frontend/src/components/layout/Header/MenuItem/index.tsx
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
 import { IMenuItem } from '@/utils/type/header';
-=======
-import { MenuItem as IMenuItem } from '@/utils/type/header';
->>>>>>> Stashed changes
 import MenuGroup from './MenuGroup';
 import MenuLink from './MenuLink';
 
