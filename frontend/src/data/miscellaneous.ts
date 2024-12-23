192eeecc34d92388d526c2e39407e02e49703205
Duke Haymich <71746413+DukeHaymich@users.noreply.github.com>
2024-12-23 17:17:42 +0700

    refactor: food data

--- frontend/src/data/miscellaneous.ts
+++ frontend/src/data/miscellaneous.ts
@@ -1,3 +1,26 @@
-const openHours = "05:45 - 23:30 hằng ngày";
+import { INumericSummaryItem } from '@/utils/type/landing';
 
-export { openHours };
+const numericSummaryData: INumericSummaryItem[] = [
+  {
+    prefix: 'Hơn',
+    numericData: 5,
+    suffix: 'năm thành lập',
+  },
+  {
+    prefix: 'Với hơn',
+    numericData: 70,
+    suffix: 'món ăn',
+    animated: true,
+  },
+  {
+    prefix: 'Thực hiện bởi',
+    numericData: 20,
+    suffixNumericData: '+',
+    suffix: 'đầu bếp chuyên nghiệp',
+    animated: true,
+  },
+];
+
+const openHours = '05:45 - 23:30 hằng ngày';
+
+export { openHours, numericSummaryData };
